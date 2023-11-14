76d0822eae732907bf9cfedd113c2a38fcf1e69a
aash100 <baranwalaashish@gmail.com>
2023-11-14 19:00:32 +0530

    Wildcard route to login page

--- banking-frontend/src/app/app.routing.ts
+++ banking-frontend/src/app/app.routing.ts
@@ -15,6 +15,7 @@ const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'home', component: HomeComponent, canActivate: [AuthGuardService]},
+    { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
